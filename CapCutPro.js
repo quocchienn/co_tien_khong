a643d5b730ba81e7c3a0f8257290231032a3c6f8
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2024-11-08 17:26:45 +0700

    Delete CapCutPro.js

--- CapCutPro.js
+++
@@ -1,9 +0,0 @@
-let obj = JSON.parse($response.body);
-
-if (obj && obj.data) {
-    obj.data.isPro = true;
-    obj.data.expireTime = "2099-12-31T23:59:59Z";
-    obj.data.subscriptionStatus = "active";
-}
-
-$done({ body: JSON.stringify(obj) });
