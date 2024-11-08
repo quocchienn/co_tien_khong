93a93f4eced6fb6124f01d4b99701a7c151d605e
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2024-11-08 17:26:23 +0700

    Delete CapCutPremium.js

--- CapCutPremium.js
+++
@@ -1,9 +0,0 @@
-let obj = JSON.parse($response.body);
-
-if (obj && obj.data) {
-    obj.data.isPremium = true;
-    obj.data.premiumExpireTime = "2099-12-31T23:59:59Z";
-    obj.data.featuresUnlocked = true;
-}
-
-$done({ body: JSON.stringify(obj) });
