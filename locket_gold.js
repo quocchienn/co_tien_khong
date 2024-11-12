3824a808668cadd28530aee15795b6a21e66a4a0
Quốc Chiến <123287600+quocchienn@users.noreply.github.com>
2024-11-12 14:03:03 +0700

    Add files via upload

---
+++ locket_gold.js
@@ -0,0 +1,6 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn!";var locket0={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2025-27-09T01:04:18Z",purchase_date:"2025-27-09T01:04:17Z",store:"app_store"},locket={grace_period_expires_date:null,purchase_date:"2025-27-09T01:04:17Z",product_identifier:"com.locket.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(locket.product_identifier=s,obj.subscriber.subscriptions[s]=locket):obj.subscriber.subscriptions["com.locket.premium.yearly"]=locket,obj.subscriber.entitlements[e]=locket01}else obj.subscriber.subscriptions["com.locket.premium.yearly"]=locket,obj.subscriber.entitlements.pro=locket;$done({body:JSON.stringify(obj)});
